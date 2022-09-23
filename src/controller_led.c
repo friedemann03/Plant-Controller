bc1aa813fda7f4e06e9fe8a001cdb7158c5926a0
Friedemann <friedemann.drepper@online.de>
2022-09-23 12:38:21 +0200

    removed most of blinky functionality, only simple blinking left

--- src/controller_led.c
+++ src/controller_led.c
@@ -9,9 +9,6 @@
 
 
 //Private Variables
-STATIC volatile bool ledEnabled = true;
-STATIC volatile bool buttonPressed = false;
-STATIC volatile bool buttonDebounced = false;
 
 STATIC uint32_t led_pins[] = {LED2_Pin};
 STATIC uint32_t led_ports[] = {LED2_GPIO_Port};
@@ -20,55 +17,16 @@ STATIC uint32_t led_ports[] = {LED2_GPIO_Port};
 void Led_Controller_Init(void) {
     Log_Message(LOG_LVL_SYSTEM, "LED Controller Starting.\n");
     Tim_EnableIRQ(true, TIMER_10);
-    Tim_EnableIRQ(false, TIMER_11);
     Tim_Enable(true, TIMER_10);
-    Tim_Enable(false, TIMER_11);
 }
 
-void Led_Controller_Update(void) {
-    if (buttonPressed) {
-        Tim_EnableIRQ(false, TIMER_11);
-        Tim_Enable(false, TIMER_11);
-
-        buttonPressed = false;
-
-        if (Gpio_Is_Input_Pin_Set(USER_BTN_GPIO_Port, USER_BTN_Pin)) {
-            buttonDebounced = true;
-        }
-    }
-
-    if (buttonDebounced) {
-        buttonDebounced = false;
-
-        if (ledEnabled) {
-            Gpio_Reset_Output_Pin(led_ports[0], led_pins[0]); // turn off currently active LED
-        }
-
-        ledEnabled = !ledEnabled;
-
-        Tim_Enable(ledEnabled, TIMER_10);
-    }
-}
 
 void Led_Controller_DeInit(void) {
     Tim_EnableIRQ(false, TIMER_10);
     Tim_EnableIRQ(false, TIMER_11);
     Gpio_Reset_Output_Pin(led_ports[0], led_pins[0]);
-    ledEnabled = false;
-    buttonPressed = false;
-}
-
-
-// Private Functions
-void Exti_15_10_Callback(void) {
-    Tim_EnableIRQ(true, TIMER_11);
-    Tim_Enable(true, TIMER_11);
 }
 
 void Tim_10_Callback(void) {
     Gpio_Toggle_Output_Pin(led_ports[0], led_pins[0]);
-}
-
-void Tim_11_Callback(void) {
-    buttonPressed = true;
 }
