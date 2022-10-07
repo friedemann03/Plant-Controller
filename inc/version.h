3941f82e33ac6adf4eb54d2de68f32bc6235e88e
Friedemann <friedemann.drepper@online.de>
2022-10-07 14:54:08 +0200

    added version module

---
+++ inc/version.h
@@ -0,0 +1,15 @@
+//
+// Created by Friedemann Drepper on 07.10.22.
+//
+
+#ifndef PLANT_CONTROLLER_VERSION_H
+#define PLANT_CONTROLLER_VERSION_H
+
+/* Includes ------------------------------------------------------------------*/
+#include "stdint.h"
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Reset_Log(void);
+
+
+#endif //PLANT_CONTROLLER_VERSION_H
