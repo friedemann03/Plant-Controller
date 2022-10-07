a3298960fd4c36564293eef991877dfe9a6d5ade
Friedemann <friedemann.drepper@online.de>
2022-10-07 13:27:20 +0200

    rewrote the led controller to use the led module

--- inc/controller_led.h
+++ inc/controller_led.h
@@ -5,17 +5,19 @@
 #ifndef YS_EXERCISE_WEEK3_CONTROLLER_LED_H
 #define YS_EXERCISE_WEEK3_CONTROLLER_LED_H
 
-#include "subsystem_gpio.h"
+#include "module_led.h"
 #include "subsystem_tim.h"
 #include "unit_testing.h"
 
 
 void Led_Controller_Init(void);
 
-void Led_Controller_Update(void);
-
 void Led_Controller_DeInit(void);
 
-void Led_Enable_Blinking(bool status);
+void Led_Controller_Enable(bool status);
+
+void Led_Controller_EnableFastMode(bool status);
+
+void Led_Controller_EnableLedOn(bool status);
 
 #endif //YS_EXERCISE_WEEK3_CONTROLLER_LED_H
