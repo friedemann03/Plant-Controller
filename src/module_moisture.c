9c627e5dfdacb5b35ca2fcac1bb443315f838773
Friedemann <friedemann.drepper@online.de>
2022-10-13 08:13:07 +0200

    implemented moisture module

--- src/module_moisture.c
+++ src/module_moisture.c
@@ -3,3 +3,19 @@
 //
 
 #include "module_moisture.h"
+
+void Moisture_Init(moistureSensor_t *me, uint8_t channel) {
+    me->currentValue = 0;
+    me->channel = channel;
+}
+
+void Moisture_Update(moistureSensor_t *me) {
+    uint16_t newValue = Adc_Get_Value(me->channel);
+    if (newValue != 0) {
+        me->currentValue = newValue;
+    }
+}
+
+uint16_t Moisture_Get_Value(moistureSensor_t *me) {
+    return me->currentValue;
+}
