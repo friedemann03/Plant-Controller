f1c8e6f208c0e6fc0ab1921b7a8939cd98b8357c
Friedemann <friedemann.drepper@online.de>
2022-10-13 09:01:10 +0200

    implemented pump module

--- inc/module_pump.h
+++ inc/module_pump.h
@@ -5,4 +5,27 @@
 #ifndef PLANT_CONTROLLER_MODULE_PUMP_H
 #define PLANT_CONTROLLER_MODULE_PUMP_H
 
+/* Includes ------------------------------------------------------------------*/
+#include "stdint.h"
+#include "stdbool.h"
+
+/* Exported macro ------------------------------------------------------------*/
+
+/* Exported types ------------------------------------------------------------*/
+typedef struct {
+    uint32_t port;
+    uint32_t pin;
+    bool status;
+} waterPump_t;
+
+/* Exported constants --------------------------------------------------------*/
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Water_Pump_Init(waterPump_t *me, uint32_t port, uint32_t pin);
+
+void Water_Pump_Enable(waterPump_t *me, bool status);
+
+bool Water_Pump_Is_Enabled(waterPump_t *me);
+
+
 #endif //PLANT_CONTROLLER_MODULE_PUMP_H
