bec4bc3b12dc1f533475974db725431308c11ade
Friedemann <friedemann.drepper@online.de>
2022-10-13 07:53:31 +0200

    very simple implementation of adc subsystem

--- inc/subsystem_adc.h
+++ inc/subsystem_adc.h
@@ -5,4 +5,12 @@
 #ifndef PLANT_CONTROLLER_SUBSYSTEM_ADC_H
 #define PLANT_CONTROLLER_SUBSYSTEM_ADC_H
 
+#include "adc.h"
+
+
+void Adc_Subsystem_Init(void);
+
+uint16_t Adc_Get_Value(void);
+
+
 #endif //PLANT_CONTROLLER_SUBSYSTEM_ADC_H
--- src/subsystem_adc.c
+++ src/subsystem_adc.c
@@ -3,3 +3,17 @@
 //
 
 #include "subsystem_adc.h"
+
+
+void Adc_Subsystem_Init(void) {
+    MX_ADC1_Init();
+}
+
+
+uint16_t Adc_Get_Value(void) {
+    HAL_ADC_Start(&hadc1);
+    HAL_ADC_PollForConversion(&hadc1, 100);
+    uint16_t result = HAL_ADC_GetValue(&hadc1);
+    HAL_ADC_Stop(&hadc1);
+    return result;
+}
