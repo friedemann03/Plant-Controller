4b09024633482a1083989f51a22303e2a408f060
Friedemann <friedemann.drepper@online.de>
2022-10-13 08:32:15 +0200

    implemented soil controller

--- inc/controller_soil.h
+++ inc/controller_soil.h
@@ -5,6 +5,23 @@
 #ifndef PLANT_CONTROLLER_CONTROLLER_SOIL_H
 #define PLANT_CONTROLLER_CONTROLLER_SOIL_H
 
+#include "module_moisture.h"
 
+
+
+void Soil_Controller_Init(void);
+
+
+uint32_t Soil_Controller_GetSoilMoisture(void);
+
+
+void Soil_Controller_Update(void);
+
+
+uint32_t Soil_Controller_Get_MoistLimit(void);
+void Soil_Controller_Set_MoistLimit(uint32_t newMoistLimit);
+
+uint32_t Soil_Controller_Get_DryLimit(void);
+void Soil_Controller_Set_DryLimit(uint32_t newDryLimit);
 
 #endif //PLANT_CONTROLLER_CONTROLLER_SOIL_H
