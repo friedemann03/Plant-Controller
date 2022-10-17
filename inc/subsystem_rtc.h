b5ec036c7d08aeca8d4e6c925efdd2981ab61f8d
Friedemann <friedemann.drepper@online.de>
2022-10-17 13:45:29 +0200

    implemented RTC subsystem

--- inc/subsystem_rtc.h
+++ inc/subsystem_rtc.h
@@ -5,4 +5,18 @@
 #ifndef PLANT_CONTROLLER_SUBSYSTEM_RTC_H
 #define PLANT_CONTROLLER_SUBSYSTEM_RTC_H
 
+#include "rtc.h"
+
+typedef struct {
+    uint8_t hours;
+    uint8_t minutes;
+    uint8_t seconds;
+} sTime_t;
+
+void Rtc_Subsystem_Init(void);
+
+void Rtc_Subsystem_SetWakeUpInterval(uint16_t IntervalInSeconds);
+
+sTime_t Rtc_Get_Time(void);
+
 #endif //PLANT_CONTROLLER_SUBSYSTEM_RTC_H
