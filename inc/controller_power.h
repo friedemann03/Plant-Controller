c8a39f1a8a818401207dec82732d6ba7f332d556
Friedemann <friedemann.drepper@online.de>
2022-10-17 10:33:50 +0200

    added power controller implementation

--- inc/controller_power.h
+++ inc/controller_power.h
@@ -5,4 +5,11 @@
 #ifndef PLANT_CONTROLLER_CONTROLLER_POWER_H
 #define PLANT_CONTROLLER_CONTROLLER_POWER_H
 
+void Power_Controller_StopMode(void);
+
+
+
+
+
+
 #endif //PLANT_CONTROLLER_CONTROLLER_POWER_H
