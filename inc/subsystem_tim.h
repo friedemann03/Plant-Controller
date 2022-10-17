f6020e5927d85f5590a238234f89c4c33d43ddeb
Friedemann <friedemann.drepper@online.de>
2022-10-17 14:11:08 +0200

    added TIM1 to Timer Subsystem

--- inc/subsystem_tim.h
+++ inc/subsystem_tim.h
@@ -9,7 +9,7 @@
 #include "stdbool.h"
 
 /* Exported macro ------------------------------------------------------------*/
-//#define TIMER_1              1
+#define TIMER_1              1
 //#define TIMER_2              2
 //#define TIMER_3              3
 //#define TIMER_4              4
