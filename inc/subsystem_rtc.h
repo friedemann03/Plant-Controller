3ebe715cd587abb2d9f82fedee6bd71d82f6af66
Friedemann <friedemann.drepper@online.de>
2022-10-17 13:03:33 +0200

    added files for rtc subsystem

---
+++ inc/subsystem_rtc.h
@@ -0,0 +1,8 @@
+//
+// Created by Friedemann Drepper on 17.10.22.
+//
+
+#ifndef PLANT_CONTROLLER_SUBSYSTEM_RTC_H
+#define PLANT_CONTROLLER_SUBSYSTEM_RTC_H
+
+#endif //PLANT_CONTROLLER_SUBSYSTEM_RTC_H
---
+++ src/subsystem_rtc.c
@@ -0,0 +1,5 @@
+//
+// Created by Friedemann Drepper on 17.10.22.
+//
+
+#include "subsystem_rtc.h"
