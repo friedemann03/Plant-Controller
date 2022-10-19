553a18b251189b410c938718f8b13ff96266d11f
Friedemann <friedemann.drepper@online.de>
2022-10-19 14:50:16 +0200

    added tests for system events

--- test/test_system_event.c
+++ test/test_system_event.c
@@ -11,7 +11,6 @@ extern Event_t latestEvent;
 Event_t testEvent = {.priority = 1, .index = 0};
 
 void test_GetLatest_shouldReturnLatestEvent(void) {
-
     latestEvent = testEvent;
 
     Event_t returnEvent = System_Event_Get_LatestEvent();
@@ -20,4 +19,45 @@ void test_GetLatest_shouldReturnLatestEvent(void) {
     TEST_ASSERT_EQUAL(1, returnEvent.priority);
 
     TEST_ASSERT_EQUAL(0xFF, latestEvent.index);
+    TEST_ASSERT_EQUAL(0, latestEvent.priority);
+}
+
+void test_GetLatest_shouldSetLatestEventToNoEvent(void) {
+    Event_t returnEvent = System_Event_Get_LatestEvent();
+
+    TEST_ASSERT_EQUAL(0xFF, latestEvent.index);
+    TEST_ASSERT_EQUAL(0, latestEvent.priority);
+}
+
+void test_TriggerEvent_shouldSetLatestEvent(void) {
+    testEvent.priority = 12;
+    testEvent.index = 13;
+
+    systemEvents[2] = testEvent;
+
+    System_Event_Trigger_Event(2);
+
+    TEST_ASSERT_EQUAL(12, latestEvent.priority);
+    TEST_ASSERT_EQUAL(13, latestEvent.index);
+}
+
+void test_TriggerEvent_shouldNotSetLatestEvent_DueToLowerOrEqualPriority(void) {
+    testEvent.priority = 12;
+    testEvent.index = 13;
+
+    systemEvents[2] = testEvent;
+
+    latestEvent.priority = 15;
+    latestEvent.index = 42;
+
+    System_Event_Trigger_Event(2);
+
+    TEST_ASSERT_EQUAL(15, latestEvent.priority);
+    TEST_ASSERT_EQUAL(42, latestEvent.index);
+
+    latestEvent.priority = 12;
+
+    System_Event_Trigger_Event(2);
+
+    TEST_ASSERT_EQUAL(42, latestEvent.index);
 }
