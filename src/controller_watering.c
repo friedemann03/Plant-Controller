2b9c5dff2abf6f5fab7e99300c1e58ece38877ed
Friedemann <friedemann.drepper@online.de>
2022-10-25 10:50:55 +0200

    added implementation for watering

--- src/controller_watering.c
+++ src/controller_watering.c
@@ -3,3 +3,27 @@
 //
 
 #include "controller_watering.h"
+#include "module_pump.h"
+#include "subsystem_gpio.h"
+#include "stm32f4xx_ll_utils.h"
+
+
+#define INTERVAL_1S 1000
+#define INTERVAL_10M 10000
+#define PUMP_ACTIVE_DELAY INTERVAL_1S
+#define WAITING_FOR_SOIL_DELAY INTERVAL_10M
+
+
+waterPump_t pump;
+
+void Watering_Controller_Init(void) {
+    Water_Pump_Init(&pump, Gpio_Port_A, Gpio_Pin_8);
+}
+
+
+void Watering_Controller_WaterPlant(void) {
+    Water_Pump_Enable(&pump, true);
+    LL_mDelay(PUMP_ACTIVE_DELAY);
+    Water_Pump_Enable(&pump, false);
+    LL_mDelay(WAITING_FOR_SOIL_DELAY);
+}
